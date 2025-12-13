9727968ec4a7f9373d5713e935c6a4d6e7d9b94e
rian144hz <rianmatheus669@gmail.com>
2025-12-13 19:58:27 -0300

    Terceira questão

---
+++ lista1/questa3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void)
+{
+    int valor;
+    printf("Digite algum valor: ");
+    scanf(" %d", &valor);
+    printf("O valor digitado foi: %d\n", valor);
+    return 0;
+}
