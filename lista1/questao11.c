aedd15336e7846a81763702133206c6d1a55f71f
rian144hz <rianmatheus669@gmail.com>
2026-01-14 10:19:12 -0300

    Décima segunda questão

--- lista1/questao11.c
+++ lista1/questao11.c
@@ -4,6 +4,7 @@ void main()
 {
     int nota1 = 0, nota2 = 0, nota3 = 0, peso1 = 2, peso2 = 3, peso3 = 5;
     double mediaPonderada = 0.0;
+    int media = 0;
 
     printf("Qual sua primeira nota: ");
     scanf(" %d", &nota1);
@@ -12,8 +13,8 @@ void main()
     printf("Qual sua terceira nota: ");
     scanf(" %d", &nota3);
 
-    mediaPonderada = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3);
+    media = mediaPonderada = ((nota1 * peso1) + (nota2 * peso2) + (nota3 * peso3)) / (peso1 + peso2 + peso3);
 
     printf("Sua média é \n");
-    printf("%f", mediaPonderada);
+    printf("%d", media);
 }
